919a7068ddd5b0faa4adb8c2aadb123f7b8524eb
zhangwei <zhangwei2017@unipus.cn>
2020-04-17 17:37:40 +0800

    首次提交

---
+++ src/main/java/com/example/generatermybatis/GeneratermybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.generatermybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratermybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratermybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/generatermybatis/GeneratermybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generatermybatis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratermybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
